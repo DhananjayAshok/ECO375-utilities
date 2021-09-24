5f9356109c34b059e6e4f3f7a5c2d88f9a9a69f4
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2021-09-24 19:52:26 -0400

    Logit and Probit regression added

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from statapy.data.dataset import DataSet
 from statapy.regression import *
 from statapy.plotting import *
 
-url = "https://www.stata.com/examples/auto.csv"
-filename = "data/alcohol.dta"
-dataset = DataSet(name="Sample", filename=filename)
+url = "https://stats.idre.ucla.edu/stat/data/hdp.csv"
+dataset = DataSet(name="Sample", url=url)
+X_cols = ["BMI", "tumorsize", "Age"]
+y_col = "remission"
+
 df = dataset.data
 
